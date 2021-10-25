54ddd4faecb73befcad76f3a2f9fe4b2f4f60c89
AlfredWangTJ <92712829+AlfredWangTJ@users.noreply.github.com>
2021-10-25 10:51:59 +0800

    Add files via upload

--- htmlcontainer.js
+++ htmlcontainer.js
@@ -7,16 +7,16 @@
 	class HTMLContainer extends HTMLElement {
 		constructor() {
 			super(); 
-			let HC = this.attachShadow({mode: "open"});
-			HC.appendChild(template.content.cloneNode(true));
+			this._shadowRoot = this.attachShadow({mode: "open"});
+			this._shadowRoot.appendChild(template.content.cloneNode(true));
 		}
 
-                onCustomWidgetAfterUpdate(oChangedProperties) {
+                onCustomWidgetAfterUpdate(changedProperties) {
 			if ("code" in changedProperties) {
 				this.getElementsByID("chc").innerHTML = changedProperties["code"];
 			}
 		    }
 	}
-	
+
 	customElements.define("sac-htmlcontainer", HTMLContainer);
 })();
