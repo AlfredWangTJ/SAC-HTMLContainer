1f35bc3fe18ca5115791275c57e5a889ada9c2ea
AlfredWangTJ <92712829+AlfredWangTJ@users.noreply.github.com>
2021-10-25 11:47:22 +0800

    Add files via upload

--- htmlcontainer.js
+++ htmlcontainer.js
@@ -1,17 +1,41 @@
-(function() { 
-	let template = document.createElement("template");
-	template.innerHTML = `
-        <div id="chc"></div>
+(function () {
+  let template = document.createElement("template");
+  template.innerHTML = `
 	`;
 
-	class HTMLContainer extends HTMLElement {
-		constructor() {
-			super(); 
-			this._shadowRoot = this.attachShadow({mode: "open"});
-			this._shadowRoot.appendChild(template.content.cloneNode(true));
-		}
+  class HTMLContainer extends HTMLElement {
+    constructor() {
+      super();
+      this._shadowRoot = this.attachShadow({ mode: "open" });
+      this._shadowRoot.appendChild(template.content.cloneNode(true));
+      this._dom;
+      this._htmlcode = "<div></div>";
+    }
 
-	}
+    get code() {
+      return this._htmlcode;
+    }
 
-	customElements.define("sac-htmlcontainer", HTMLContainer);
+    set code(val) {
+      this._htmlcode = val;
+    }
+
+    onCustomWidgetAfterUpdate(oChangedProperties) {
+	this.changeHTML("<p>ABCD</p>");
+    }
+
+    changeHTML(val){
+	    if(this._dom){
+		    this._dom.parentNode.removeChild(this._dom);
+	    }
+
+	    //var shadow = window.getSelection(this._shadowRoot);
+	    this._dom = document.createElement("div");
+	    this._dom.innerHTML = val;
+	    this._shadowRoot.appendChild(this._dom);
+
+    }
+  }
+
+  customElements.define("sac-htmlcontainer", HTMLContainer);
 })();
